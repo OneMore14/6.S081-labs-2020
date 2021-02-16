8830180af7638215ee59cfb70e63ebe9c2337c54
OneMore14 <sc_sunjian@163.com>
2021-02-16 16:22:42 +0800

    lab1 done

--- kernel/vm.c
+++ kernel/vm.c
@@ -440,3 +440,25 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void vmdetailprint(pagetable_t pagetable, int level) {
+  if (level > 3) {
+    return;
+  }
+  for (int i = 0; i < 512; i++) {
+    pte_t pte = pagetable[i];
+    if (pte & PTE_V) {
+      printf("..");
+      for (int j = 1; j < level; j++) {
+        printf(" ..");
+      }
+      printf("%d: pte %p pa %p\n", i, pte, PTE2PA(pte));
+      vmdetailprint((pagetable_t)PTE2PA(pte), level + 1);
+    }
+  }
+}
+
+void vmprint(pagetable_t pagetable) {
+    printf("page table %p\n", pagetable);
+    vmdetailprint(pagetable, 1);
+}
